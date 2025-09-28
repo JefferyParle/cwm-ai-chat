bbff1d704d124a83e23ba94810a1700b48d0d37f
Jeffery Parle <jefferyparledev@gmail.com>
2025-09-28 15:49:29 -0700

    Made chatbot feature

---
+++ packages/client/src/components/ChatBot.tsx
@@ -0,0 +1,71 @@
+import { useRef, useState, type KeyboardEvent } from 'react';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import { Button } from './ui/button';
+import { FaArrowUp } from 'react-icons/fa';
+
+interface FormData {
+  prompt: string;
+}
+
+interface ChatReponse {
+  message: string;
+}
+
+const ChatBot = () => {
+  const [messages, setMessages] = useState<string[]>([]);
+  const conversationId = useRef(crypto.randomUUID());
+  const { register, handleSubmit, reset, formState } = useForm<FormData>();
+
+  const onSubmit = async ({ prompt }: FormData) => {
+    setMessages((prev) => [...prev, prompt]);
+
+    reset();
+
+    const { data } = await axios.post<ChatReponse>('/api/chat', {
+      prompt,
+      conversationId: conversationId.current,
+    });
+    setMessages((prev) => [...prev, data.message]);
+  };
+
+  const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit(onSubmit)();
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        {messages.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))}
+      </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        onKeyDown={onKeyDown}
+        className="flex flex-col gap-2 items-end relative"
+      >
+        <textarea
+          {...register('prompt', {
+            required: true,
+            validate: (data) => data.trim().length > 0,
+          })}
+          className="w-full min-h-40 border-2 p-4 rounded-3xl focus:outline-0 resize-none"
+          placeholder="Ask anything"
+          maxLength={1000}
+        />
+        <Button
+          disabled={!formState.isValid}
+          className="rounded-full w-9 h-9 right-4 bottom-4 absolute cursor-pointer"
+        >
+          <FaArrowUp />
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatBot;
