6fb4cf572ed246951f46b182976e4b4869cfcdbe
Jeffery Parle <jefferyparledev@gmail.com>
2025-09-28 20:31:34 -0700

    Extracted chat messages component

---
+++ packages/client/src/components/chat/ChatMessages.tsx
@@ -0,0 +1,44 @@
+import ReactMarkdown from 'react-markdown';
+import { useEffect, useRef, type ClipboardEvent } from 'react';
+
+interface ChatMessagesProps {
+  messages: Message[];
+}
+
+export interface Message {
+  content: string;
+  role: 'user' | 'bot';
+}
+
+const ChatMessages = ({ messages }: ChatMessagesProps) => {
+  const lastMessageRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
+
+  const onCopyMessage = (e: ClipboardEvent<HTMLDivElement>) => {
+    const selection = window.getSelection()?.toString().trim();
+    if (selection) {
+      e.preventDefault();
+      e.clipboardData.setData('text/plain', selection);
+    }
+  };
+
+  return (
+    <>
+      {messages.map((message, index) => (
+        <div
+          key={index}
+          onCopy={onCopyMessage}
+          ref={index === messages.length - 1 ? lastMessageRef : null}
+          className={`px-4 py-2 rounded-2xl ${message.role === 'user' ? 'bg-blue-600 text-white self-end' : 'bg-gray-100 text-black self-start'}`}
+        >
+          <ReactMarkdown>{message.content}</ReactMarkdown>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default ChatMessages;
